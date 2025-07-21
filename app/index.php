80a30039376306f5ec75e58b7bf12890e669512c
Jurandir Junior <jurandir.batistaljr@gmail.com>
2025-07-21 16:55:47 -0300

    feat: Enhance container creation with n8n configuration and add evolution-api and n8n templates

--- app/index.php
+++ app/index.php
@@ -41,8 +41,45 @@ try {
                 $result = $docker->restartContainer($matches[1]);
                 echo json_encode($result);
             } elseif ($path === '/containers/create') {
-                $result = $docker->createContainer($input['config'], $input['name'] ?? null);
+                $software = $input['software'] ?? null;
+                $client = $input['client'] ?? null;
+
+                $client = substr(trim(str_replace(' ','',strtolower($client))), 0, 15);
+
+                $vcpu = $input['vcpus'] ?? 1;
+                $memory = $input['memory'] ?? 512; // Default to 512MB
+
+                $subdomain = $software.'-'.$client.'-'.uniqid();
+
+                if (!is_dir('/etc/automation-webhook/volumes/'.$client)) {
+                    mkdir('/etc/automation-webhook/volumes/'.$client, 0755, true);
+                }
+
+                if($software === 'n8n')
+                {
+                    $templatePath = '../templates/n8n.yml';
+                    $destinationPath = '/etc/automation-webhook/volumes/' . $client . '/n8n.yml';
+                    
+                    if (file_exists($templatePath)) {
+                        copy($templatePath, $destinationPath);
+                    }
+
+                    $envTemplatePath = '../templates/n8n.env';
+                    $envDestinationPath = '/etc/automation-webhook/volumes/' . $client . '/.env';
+
+                    if (file_exists($envTemplatePath)) {
+                        $envContent = file_get_contents($envTemplatePath);
+                        $envContent = str_replace('{SUBDOMAIN}', $subdomain, $envContent);
+                        file_put_contents($envDestinationPath, $envContent);
+                    }
+
+                    $result = $docker->createContainer($software, $client, $vcpu, $memory, $subdomain);
+                }
+
                 echo json_encode($result);
+            } else {
+                http_response_code(400);
+                echo json_encode(['error' => 'Invalid endpoint']);
             }
             break;
             

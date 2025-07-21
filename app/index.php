ed9dd4be1f84f9559bb41f398754b01e73d140a7
Jurandir Junior <jurandir.batistaljr@gmail.com>
2025-07-21 15:58:55 -0300

    api test

--- app/index.php
+++ app/index.php
@@ -4,3 +4,9 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type');
+
+
+echo "{
+    \"status\": \"success\",
+    \"message\": \"API is running\"
+}";
